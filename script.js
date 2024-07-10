a6405feaf08d1661dcf0ecf9043e50d2a4723150
VictoriaMelo <95241371+VictoriaMelo@users.noreply.github.com>
2024-07-10 15:04:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Seu código aqui
 });
+
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    const activeSection = document.getElementById(sectionId);
+    if (activeSection) {
+        activeSection.style.display = 'block';
+    }
+}
