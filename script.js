7219d11a4b9da1fe49d99d4c293b69d55e7f7a34
VictoriaMelo <95241371+VictoriaMelo@users.noreply.github.com>
2024-07-11 11:22:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Adicione aqui os scripts necessários
 document.addEventListener('DOMContentLoaded', () => {
     showSection('identification');
+    var map = L.map('map').setView([41.79662103554626, -6.767477806574788], 18);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([41.79662103554626, -6.767477806574788]).addTo(map)
+        .bindPopup('Instituto Politécnico de Bragança')
+        .openPopup();
 });
 
 function showSection(sectionId) {
